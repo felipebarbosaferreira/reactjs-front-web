8f7770d27b82b2a7abe80fd47ab5ecf86c5682b3
C1281192 Felipe Barbosa Ferreira <c1281192@interno.bb.com.br>
2020-05-23 21:01:47 -0300

    ajustes no Css do text do card da tarefa

--- src/components/TaskCard/styles.js
+++ src/components/TaskCard/styles.js
@@ -5,13 +5,14 @@ import styled from 'styled-components';
  */
 
 export const Container = styled.div`
-    width: 270px;
+    width: 230px;
+    max-width: 230px;
     height: 140px;
     background: #FFFFFF;
     box-shadow: 0px 0px 11px 0px rgba(117,117,117,1);
     border-radius: 10px;
     margin: 30px;
-    padding: 20px 0px 10px 0px;
+    padding: 20px 5px 10px 5px;
     
     cursor: pointer;
     transition: all 0.3s ease;
@@ -21,7 +22,7 @@ export const Container = styled.div`
     justify-content: space-between;
     flex-direction: column;
 
-    opacity: ${props => props.done ? 0.5 : 1 };
+    opacity: ${props => props.done ? 0.5 : 1};
 
     svg {
         color: #EE6B26;
@@ -37,6 +38,18 @@ export const TopCard = styled.div`
     align-items: center;
     justify-content: center;
     flex-direction: column;
+    text-align: center;
+
+    h3 {
+        margin: 5px 0px 0px 0px;
+        line-height: 1.5em; /* Sets line height to 1.5 times text size */
+        height: 3em; /* Sets the div height to 2x line-height (3 times text size) */
+        width: 100%; /* Use whatever width you want */
+        white-space: normal; /* Wrap lines of text */
+        overflow: hidden; /* Hide text that goes beyond the boundaries of the div */
+        text-overflow: ellipsis; /* Ellipses (cross-browser) */
+        -o-text-overflow: ellipsis; /* Ellipses (cross-browser) */
+    }
 `;
 
 export const BottomCard = styled.div`
